3906d16d96e65ddedd24418edfc87188f34e99cb
aj25687 <javantika.jain@gmail.com>
2025-11-16 08:41:21 -0500

    testing2

--- Reverse.java
+++ Reverse.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Reverse{
 
     public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        
+        scan.nextLine();
 
         System.out.println("reverse"); 
         System.out.println("test"); 
