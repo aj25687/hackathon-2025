f7f1405648575503808d6a3dc61d50ab7dbbb629
aj25687 <javantika.jain@gmail.com>
2025-11-16 08:48:56 -0500

    added a file

--- Reverse.java
+++ Reverse.java
@@ -1,17 +1,29 @@
+import java.io.File;                  // Import the File class
+import java.io.FileNotFoundException; // Import this class to handle errors
 import java.util.Scanner;
-
 public class Reverse{
 
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
-        
+
         scan.nextLine();
 
         System.out.println("reverse"); 
         System.out.println("test"); 
-        //test
-        //test
         System.out.println("oliver");
 
+        File myObj = new File("filename.txt");
+
+    // try-with-resources: Scanner will be closed automatically
+    try (Scanner myReader = new Scanner(myObj)) {
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+    } catch (FileNotFoundException e) {
+      System.out.println("An error occurred.");
+      e.printStackTrace();
     }
+    }
+
 }
